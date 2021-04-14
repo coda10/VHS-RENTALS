91381cb7e076aed3e29c7c2a3f8d99b651cae6e1
Francis Badasu <nytro04@gmail.com>
2021-04-14 12:38:15 +0000

    I finished creating admin Login interface

--- frontend/vhs/src/app/adminlogin/adminlogin.component.ts
+++ frontend/vhs/src/app/adminlogin/adminlogin.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AdminauthService } from '../adminauth.service';
 
 @Component({
   selector: 'app-adminlogin',
@@ -6,8 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminlogin.component.css']
 })
 export class AdminloginComponent implements OnInit {
+  
+  serverFeedBack: any;
 
-  constructor() { }
+  loginForm = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required)
+  });
+
+  login(loginForm: any){
+    console.log(loginForm.value);
+    //login
+    this.http.loginAdmin(loginForm.value).subscribe(data => this.serverFeedBack = data );
+    //console.log(HttpErrorResponse.);
+    
+  }
+
+  constructor(private http: AdminauthService) { }
 
   ngOnInit(): void {
   }
